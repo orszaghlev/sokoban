b3f5aaeade557fe577375a1d51c79d674becc317
whoisZORZ <orszaghlev@gmail.com>
2020-05-15 14:53:23 +0200

    Added game results package and its classes

---
+++ src/main/java/game/results/GameResultExample.java
@@ -0,0 +1,20 @@
+package game.results;
+
+import java.time.Duration;
+
+public class GameResultExample {
+
+    public static void main(String[] args) {
+        GameResultDao gameResultDao = GameResultDao.getInstance();
+        GameResult gameResult = GameResult.builder()
+                .player("orszaghlev")
+                .solved(true)
+                .steps(36)
+                .duration(Duration.ofMinutes(3))
+                .build();
+        gameResultDao.persist(gameResult);
+        System.out.println(gameResult);
+        System.out.println(gameResultDao.findBest(5));
+    }
+
+}
