7c3e26bb65f2a7f6b0ab4e844243a91b78d56d39
whoisZORZ <orszaghlev@gmail.com>
2020-05-21 20:29:34 +0200

    Implemented testOf() actor test

--- src/test/java/game/state/ActorTest.java
+++ src/test/java/game/state/ActorTest.java
@@ -4,7 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ActorTest {
+class ActorTest {
 
+    @Test
+    void testOf() {
+        assertThrows(IllegalArgumentException.class, () -> Actor.of(-1));
+        assertEquals(Actor.EMPTY, Actor.of(0));
+        assertEquals(Actor.WALL, Actor.of(1));
+        assertEquals(Actor.CHARACTER, Actor.of(2));
+        assertEquals(Actor.BALL, Actor.of(3));
+        assertEquals(Actor.STORAGE, Actor.of(4));
+        assertThrows(IllegalArgumentException.class, () -> Actor.of(5));
+    }
 
 }
