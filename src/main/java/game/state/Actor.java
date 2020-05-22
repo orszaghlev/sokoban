d0f724b9a731eaff9bbd8977c5c4f89abff80109
whoisZORZ <orszaghlev@gmail.com>
2020-05-22 21:42:00 +0200

    Reimplemented Actor.java based on the provided example project

--- src/main/java/game/state/Actor.java
+++ src/main/java/game/state/Actor.java
@@ -1,124 +1,74 @@
 package game.state;
 
-import javafx.scene.image.Image;
-
 /**
- * The Actor class creates the functionality of the actors in the game:
+ * The Actor class represents the actors in the game:
  * the ball, the storage, the wall and the character.
  * @author orszaghlev
  */
+public enum Actor {
 
-public class Actor {
-
-    public static final int SPACE = 64;
-
-    public static int x;
-    public static int y;
-    public static Image image;
+    EMPTY,
+    WALL,
+    CHARACTER,
+    BALL,
+    STORAGE;
 
     /**
-     * The constructor of an actor in the game.
-     * @param x the x coordinate of the actor
-     * @param y the y coordinate of the actor
+     * The array defining the transitions between orientations when a character is
+     * moved. Rows correspond to cube orientations, columns correspond to
+     * directions.
      */
-
-    public Actor(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+    private static final int[][] T = {
+            {0, 0, 0, 0},
+            {3, 4, 2, 5},
+            {1, 2, 6, 2},
+            {6, 3, 1, 3},
+            {4, 6, 4, 1},
+            {5, 1, 5, 6},
+            {2, 5, 3, 4}
+    };
 
     /**
+     * Returns the instance represented by the value specified.
      *
-     * @return the image of the actor
+     * @param value the value representing an instance
+     * @return the instance represented by the value specified
+     * @throws IllegalArgumentException if the value specified does not
+     * represent an instance
      */
-
-    public Image getImage() {
-        return image;
+    public static Actor of(int value) {
+        if (value < 0 || value >= values().length) {
+            throw new IllegalArgumentException();
+        }
+        return values()[value];
     }
 
     /**
+     * Returns the integer value that represents this instance.
      *
-     * @param img the image to set
+     * @return the integer value that represents this instance
      */
-
-    public void setImage(Image img) {
-        image = img;
+    public int getValue() {
+        return ordinal();
     }
 
     /**
+     * Moves the character to the direction specified.
      *
-     * @return the x coordinate of the actor
+     * @param direction the direction to which the character is moved
+     * @return the character moved to the direction specified
+     * @throws UnsupportedOperationException if the method is invoked on the
+     * {@link #EMPTY} instance.
      */
-
-    public static int x() {
-        return x;
+    public Actor moveTo(Direction direction) {
+        if (this == WALL) {
+            throw new UnsupportedOperationException();
+        }
+        return values()[T[ordinal()][direction.ordinal()]];
     }
 
-    /**
-     *
-     * @return the y coordinate of the actor
-     */
-
-    public static int y() {
-        return y;
-    }
-
-    /**
-     *
-     * @param x the x coordinate to set
-     */
-
-    public void setX(int x){
-        this.x = x;
-    }
-
-    /**
-     *
-     * @param y the y coordinate to set
-     */
-
-    public void setY(int y){
-        this.y = y;
-    }
-
-    /**
-     * Decides if the <code>actor</code> collides with another actor to the left.
-     * @param actor the current actor
-     * @return true or false
-     */
-
-    public static boolean isLeftCollision(Actor actor) {
-        return x() - SPACE == actor.x() && y() == actor.y();
-    }
-
-    /**
-     * Decides if the <code>actor</code> collides with another actor to the right.
-     * @param actor the current actor
-     * @return true or false
-     */
-
-    public static boolean isRightCollision(Actor actor) {
-        return x() + SPACE == actor.x() && y() == actor.y();
-    }
-
-    /**
-     * Decides if the <code>actor</code> collides with another actor to the top.
-     * @param actor the current actor
-     * @return true or false
-     */
-
-    public static boolean isTopCollision(Actor actor) {
-        return y() - SPACE == actor.y() && x() == actor.x();
-    }
-
-    /**
-     * Decides if the <code>actor</code> collides with another actor to the bottom.
-     * @param actor the current actor
-     * @return true or false
-     */
-
-    public static boolean isBottomCollision(Actor actor) {
-        return y() + SPACE == actor.y() && x() == actor.x();
+    public String toString() {
+        return Integer.toString(ordinal());
     }
 
 }
